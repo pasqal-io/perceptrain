21dc0c01365168dbfa826c5b3d77f061c13065e5
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2025-05-14 16:57:09 +0200

    [example] add custom loss w grad-weights

--- examples/ode_adaptive_loss.py
+++ examples/ode_adaptive_loss.py
@@ -6,7 +6,7 @@
 
 from __future__ import annotations
 
-from typing import Callable
+from typing import Callable, Iterator
 
 import torch
 
@@ -31,6 +31,79 @@ class FFNN(torch.nn.Module):
         return self.nn(x)
 
 
+def mse(batch_values: torch.Tensor) -> torch.Tensor:
+    return torch.mean(torch.sum(batch_values**2, dim=1), dim=0)
+
+
+class GradWeightedLoss:
+    def __init__(
+        self,
+        batch: dict[str, torch.Tensor],
+        optimizer: torch.optim.Optimizer,
+        gradient_weights: dict[str, float | torch.Tensor],
+        fixed_metric: str,
+        alpha: float = 0.9,
+    ):
+        self.metric_names = batch.keys()
+        self.gradient_weights = gradient_weights
+        self.gradients = {key: {} for key in self.metric_names}
+        self.optimizer = optimizer
+        self.fixed_metric = fixed_metric
+        self.alpha = alpha
+
+    def _update_metrics_gradients(
+        self,
+        metrics: dict[str, torch.Tensor],
+        model_parameters: Iterator[tuple[str, torch.nn.parameter.Parameter]],
+    ) -> None:
+        self.optimizer.zero_grad()
+        for key, metric in metrics.items():
+            metric.backward(retain_graph=True)
+            self.gradients[key] = {
+                name: torch.clone(param.grad.flatten())
+                for name, param in model_parameters
+                if param.grad is not None
+            }
+
+    def _gradient_norm_weighting(
+        self, metrics: dict[str, torch.Tensor]
+    ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+        fixed_grad = self.gradients[self.fixed_metric]
+        fixed_dthetas = torch.cat(tuple(dlayer for dlayer in fixed_grad.values()))
+        #
+        # get max absolute gradient corresponding to residual loss term
+        max_grad = fixed_dthetas.abs().max()
+
+        # calculate weights for IC and BC terms
+        for key, val in self.gradients.items():
+            if key != self.fixed_metric:
+                mean_grad = torch.cat(list(val.values())).abs().mean()
+                self.gradient_weights[key] = (1.0 - self.alpha) * self.gradient_weights[
+                    key
+                ] + self.alpha * max_grad / mean_grad
+
+        # calculate reweighted loss and metrics
+        reweighted_metrics = {key: val * self.gradient_weights[key] for key, val in metrics.items()}
+        reweighted_loss = torch.sum(torch.stack([val for val in reweighted_metrics.values()]))
+
+        return reweighted_loss, reweighted_metrics
+
+    @staticmethod
+    def _compute_unweighted_metrics_and_loss(features: dict[str, torch.Tensor]):
+        metrics = {name: mse(feature) for name, feature in features.items()}
+        loss = sum(metrics.values())
+        return loss, metrics
+
+    def __call__(
+        self, model: torch.nn.Module, batch: dict[str, torch.Tensor]
+    ) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:
+        loss, metrics = self._compute_unweighted_metrics_and_loss(model(batch))
+        self._update_metrics_gradients(metrics, model.named_parameters())
+        loss, metrics = self._gradient_norm_weighting(metrics)
+
+        return loss, metrics
+
+
 def main():
     def ode_rhs(x: torch.Tensor) -> torch.Tensor:
         return 5 * (4 * x**3 + x**2 - 2 * x - 0.5)
@@ -46,18 +119,6 @@ def main():
             retain_graph=True,
         )[0]
         return grad
-
-    # def loss_function(model: torch.nn.Module, data: torch.Tensor) -> tuple[torch.Tensor, dict]:
-    #     loss, metrics = None, {}
-    #     # we need access to individual loss terms - call backward() in loop
-    #     for key, metric in metrics.items():
-    #         metric.backward(retain_graph=True)
-    #         gradients[key] = {
-    #             name: torch.clone(param.grad.flatten())
-    #             for name, param in model.named_parameters()
-    #             if param.grad is not None
-    #         }
-    #     return loss, metrics
 
 
 if __name__ == "__main__":
