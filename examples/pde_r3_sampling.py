d87aebb7d25fd420222b10cd165240fea0f4c536
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2025-05-16 17:28:17 +0200

    [example-r3-sampling] Add empty file with description

---
+++ examples/pde_r3_sampling.py
@@ -0,0 +1,18 @@
+# Aim: train a neural network to learn the solution of the advection equation.
+# Uses the R3 sampling technique in https://arxiv.org/abs/2001.04536.
+#
+# du/dt + \beta(du/dx) = 0
+# u(t=0, x) = h(x)
+# u(t, x=0) = u(t, x=2\pi)
+# \Omega = [0, 1] X [0, 2\pi]
+#
+
+from __future__ import annotations
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
