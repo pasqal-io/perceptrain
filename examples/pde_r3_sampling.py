db8ec6549697c7c424d00889adc3850b2dbdc329
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2025-05-20 17:16:11 +0200

    [Example/R3] Add R3 sampling callback

--- examples/pde_r3_sampling.py
+++ examples/pde_r3_sampling.py
@@ -10,6 +10,22 @@
 from __future__ import annotations
 
 import argparse
+from typing import Callable
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import TensorDataset
+
+from perceptrain.callbacks import Callback
+from perceptrain.data import DictDataLoader, InfiniteTensorDataset
+from perceptrain.loss.loss import MSELoss
+from perceptrain.types import Loss
+
+"""The R3 logic should be a callback.
+
+Generalize to resampling from a continuous proba dist (or actually a function,
+which does not need to be in (0, 1) and integrate to 1.
+"""
 
 
 def parse_arguments() -> argparse.Namespace:
@@ -26,6 +42,78 @@ def parse_arguments() -> argparse.Namespace:
     )
     args = parser.parse_args()
     return args
+
+
+class R3Sampling(Callback):
+    def __init__(
+        self,
+        initial_dataset: TensorDataset | InfiniteTensorDataset,
+        label_function: Callable[[torch.Tensor], torch.Tensor],
+        fitness_function: Loss = MSELoss(),
+        threshold: float = 0.1,
+        sampling_distribution: Callable[[int], torch.Tensor] | None = None,
+        dataloader_key: str | None = None,
+    ):
+        """Note that only the first tensor in the dataset is considered, and it is assumed to be.
+
+        the tensor of features.
+
+        We pass the dataset, not the single tensors, because the object is more general, because
+        map/iterable-style are chosen upstream and because we can use the init constructor of
+        datasets.
+        Assumes supervised learning (labels).
+        """
+        self.dataset = initial_dataset
+        self.features = initial_dataset.tensors[0]  # NOTE constraint on the dataset
+        self.labels = initial_dataset.tensors[1]  # NOTE constraint on the dataset
+        self.label_function = label_function
+
+        self.n_samples_total = len(self.features)
+        self.n_features = initial_dataset.tensors[0].size(dim=1)
+        self.threshold = threshold
+        self.fitness_function = fitness_function
+        self.sampling_distribution = sampling_distribution
+        self.dataloader_key = dataloader_key
+
+        self.n_retained = 0
+        super().__init__(on="train_epoch_start")
+
+    def _sample_uniform(self, n_samples: int) -> torch.Tensor:
+        """Random uniform sampling in [0, 1]."""
+        return torch.rand(size=(n_samples, self.n_features))
+
+    def run_callback(self, trainer: Any, config: TrainConfig, writer: BaseWriter) -> None:
+        """Eventually make a new dataloader from a dataset.__init__() call."""
+        # Compute fitness function on all samples
+        fitnesses, _ = self.fitness_function(trainer.model, (self.features, self.labels))
+
+        # Retain
+        retained = fitnesses > self.threshold
+        self.n_retained = len(retained)
+
+        # Resample
+        new_features = self._sample_uniform(n_samples=self.n_samples_total - self.n_retained)
+
+        # Release
+        self.features = torch.where(retained, self.features, new_features)
+
+        # Compute labels
+        self.labels = self.label_function(self.features)
+
+        # Re-init dataset
+        self.dataset.__init__(self.features, self.labels)
+
+        # Update dataloader of the trainer
+        if isinstance(trainer.dataloader, DataLoader):
+            trainer.dataloader.dataset = self.dataset
+        elif isinstance(trainer.dataloader, DictDataLoader):
+            if self.dataloader_key is not None:
+                trainer.dataloader.dataloaders[self.dataloader_key].dataset = self.dataset
+            else:
+                raise ValueError(
+                    "Updating a dictdataloader is not possible,"
+                    "unless the key of the dataloader to be updated is specified."
+                )
 
 
 def main():
