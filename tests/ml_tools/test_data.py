182bcb78288711c53451e8ef791e4e7b61db14bc
Giorgio Tosti Balducci <giorgio.tosti-balducci@pasqal.com>
2025-06-10 10:15:08 +0200

    [Test] getitem of r3 dataset

--- tests/ml_tools/test_data.py
+++ tests/ml_tools/test_data.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import random
 from typing import Callable
 
 import pytest
+from torch import Tensor
 
 
 @pytest.mark.parametrize("num_samples", [100, 0])
@@ -20,3 +22,11 @@ def test_r3_dataset_init_invalid_threshold(make_mock_r3_dataset: Callable) -> No
 def test_r3_dataset_len(num_samples: int, make_mock_r3_dataset: Callable) -> None:
     dataset = make_mock_r3_dataset(num_samples)
     assert len(dataset) == num_samples
+
+
+def test_r3_dataset_getitem(make_mock_r3_dataset: Callable) -> None:
+    dataset = make_mock_r3_dataset()
+    for _ in range(3):
+        idx = random.randint(0, len(dataset) - 1)
+        assert len(dataset[idx]) == 1
+        assert isinstance(dataset[idx][0], Tensor)
